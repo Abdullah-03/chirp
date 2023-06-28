824680e84862f8bc18cba22afd40fc50c7316511
Darana <abdullahsaud03@gmail.com>
2023-06-28 15:14:40 +0500

    Added tRPC router to query our posts on external db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 export default function Home() {
+  const { data } = api.posts.getAll.useQuery();
   const user = useUser();
   return (
     <>
-      {!user.isSignedIn && <SignInButton />}
-      {!!user.isSignedIn && <SignOutButton />}
+      <div>
+        {!user.isSignedIn && <SignInButton />}
+        {!!user.isSignedIn && <SignOutButton />}
+      </div>
+
+      <div>
+        {data?.map((post) => (
+          <h1 key={post.id}>{post.content}</h1>
+        ))}
+      </div>
     </>
   );
 }
